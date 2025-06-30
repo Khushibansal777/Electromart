60f878bbdfeb4504934a446e9112c6c462b5868f
Khushi <khushibansal787@gmail.com>
2025-06-30 21:03:39 +0530

    Connected frontend to backend API

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -13,7 +13,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://electromart-server-eh9w.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
