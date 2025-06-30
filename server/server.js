226eb8ead971f2f92c4759ff906bbce44d11f58f
Khushi <khushibansal787@gmail.com>
2025-06-30 21:22:15 +0530

    Fix: cors

--- server/server.js
+++ server/server.js
@@ -16,25 +16,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-// app.use(cors());
-const allowedOrigins = [
-  "https://electromart-phdr.vercel.app",
-  "https://electromart-phdr-7k2fg9som-khushi-bansals-projects-35607dc9.vercel.app",
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 
 // API endpoints
